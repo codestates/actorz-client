4cce5d58b36849b2d5ea2cf6ac8dacacc4a5bc6c
jiweon21 <jna215843@gmail.com>
2021-06-25 01:01:21 +0900

    create setting

---
+++ actorz/src/App.js
@@ -0,0 +1,19 @@
+import Nav from "./components/Nav";
+import Intropage from "./pages/Intropage";
+import Mainpage from "./pages/Mainpage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Intropage} />
+        <Switch>
+          <Route path="/mainpage" component={Mainpage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
