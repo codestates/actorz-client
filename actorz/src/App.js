11097e8e045a5b7b21b51300b2a3c1a7e696e340
jiweon21 <jna215843@gmail.com>
2021-06-26 14:49:17 +0900

    add:postupload

--- actorz/src/App.js
+++ actorz/src/App.js
@@ -3,8 +3,22 @@ import Intropage from "./pages/Intropage";
 import Mainpage from "./pages/Mainpage";
 import "./App.css"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import FileUpload from "./components/file-upload/file-upload.component"
 
 function App() {
+  const [newUserInfo, setNewUserInfo] = useState({
+    profileImages: []
+  });
+
+  const updateUploadedFiles = (files) =>
+    setNewUserInfo({ ...newUserInfo, profileImages: files });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //logic to create new user...
+  };
+
   return (
     <Router>
       <div>
@@ -12,6 +26,15 @@ function App() {
         <Switch>
           <Route path="/mainpage" component={Mainpage} />
         </Switch>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <FileUpload
+            accept=".jpg,.png,.jpeg, .mp4"
+            multiple
+            updateFilesCb={updateUploadedFiles}
+          />
+        </form>
       </div>
     </Router>
   );
--- actorz/src/index.js
+++ actorz/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Postupload from "./components/file-upload/file-upload.component";
 
 ReactDOM.render(
   <React.StrictMode>
