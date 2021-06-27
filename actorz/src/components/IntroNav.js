8ada2d1000f37f8981b104716cc88d115281d547
Jack Lim <jennystr233@gmail.com>
2021-06-27 18:57:23 +0900

    update intropage

--- actorz/src/components/IntroNav.js
+++ actorz/src/components/IntroNav.js
@@ -4,14 +4,9 @@ import Signup from "./Signup";
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
 
-const Nav = () => {
-  const [search, setSearch] = useState("");
+const IntroNav = () => {
   const [clickSignin, setClickSignin] = useState(false);
   const [clickSignup, setClickSignup] = useState(false);
-
-  const inputHandler = (event) => {
-    setSearch(event.target.value);
-  };
 
   const handleClickSignin = () => {
     setClickSignup(false);
@@ -36,21 +31,11 @@ const Nav = () => {
       <div id="nav">
         <span className="title">Actorz</span>
 
-        <div className="search">
+        <div className="introNavbar">
           <div>
             <a href='https://www.naver.com' target='_blank'>
               <img src="https://media.vlpt.us/images/iooi75/post/4f4baec2-cb29-4b8b-b2ea-892cef41febc/Screen%20Shot%202021-06-22%20at%209.56.14%20PM.png" className="headerLogo" />
             </a>
-          </div>
-
-          <div>
-            <input
-              className="product-search"
-              value={search}
-              placeholder="  search..."
-              onChange={e => inputHandler(e)}
-            ></input>
-            <Button variant="outlined" className="product-search-btn">검색</Button>
           </div>
 
           <div className="signBtnPosition">
@@ -69,4 +54,4 @@ const Nav = () => {
   );
 };
 
-export default Nav;
+export default IntroNav;
--- actorz/src/pages/Intropage.js
+++ actorz/src/pages/Intropage.js
@@ -5,12 +5,14 @@ import '../App.css'
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
 import { GithubOutlined, ToolOutlined, InstagramOutlined, FormOutlined, FacebookOutlined, YoutubeOutlined, VerticalAlignBottomOutlined, ArrowDownOutlined} from '@ant-design/icons';
+import IntroNav from "../components/IntroNav";
 
 const Intropage = () => {
   return (
     <>
       <div className="backgroundImage">
-        <Nav /> {/* navigation bar 상단에 고정시키려면 css에서 position:fixed 하면 됨*/}
+        <IntroNav />{/* navigation bar 상단에 고정시키려면 css에서 position:fixed 하면 됨*/}
+        <div className="blockPosition"></div>
         <div className="introContainer">
           <div id="logoPosition">
             <img src="https://media.vlpt.us/images/iooi75/post/08503e87-6d66-4e92-948b-58988f10b27a/Screen%20Shot%202021-06-22%20at%209.24.46%20PM.png" alt=""
@@ -33,6 +35,21 @@ const Intropage = () => {
         </div>
       </div>
 
+      <div className="blockPosition2">
+        <div className="blockPosition3"> </div>
+        <div className="middleContents"> </div>
+        <div className="blockPosition4">- </div>
+        <div className="middleContents2"> 
+          <div className="1995Position">
+            <img src="https://media.vlpt.us/images/iooi75/post/2b4cc4a3-a7b7-42e6-b1df-516c4cf09264/image.png" alt=""
+              className="1995"
+            />
+          </div>
+        </div>
+      </div>
+
+
+
       <div className="button2Position">
         <Link to="/mainpage">
           <Button type="primary" className="startButton2" danger>시작하기</Button>
@@ -41,36 +58,55 @@ const Intropage = () => {
       <br></br><br></br><br></br><br></br>
 
 
+
       <div id="initFooter">
-        <ToolOutlined className="footerIcon" />
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <span className="footerName">김선들 |</span>
-        <GithubOutlined className="footerIcon" />
-        <FormOutlined className="footerIcon" />
-        <InstagramOutlined className="footerIcon" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div className="footerUser1">
+          <ToolOutlined className="footerIcon" />
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <span className="footerName">김선들 |</span>
+          <a href='https://github.com/SundeulDonaKim' target='_blank'>
+            <GithubOutlined className="footerIcon" />
+          </a>
+          <a href='https://velog.io/@dandelion' target='_blank'>
+          <FormOutlined className="footerIcon" />
+          </a>
+          <InstagramOutlined className="footerIcon" />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
 
-        <span className="footerName">이한빈 |</span>
-        <GithubOutlined className="footerIcon" />
-        <FormOutlined className="footerIcon" />
-        <InstagramOutlined className="footerIcon" />
-        <FacebookOutlined className="footerIcon" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div className="footerUser2">
+          <span className="footerName">이한빈 |</span>
+          <a href='https://github.com/lhb7021' target='_blank'>
+            <GithubOutlined className="footerIcon" />
+          </a>
+          <a href='https://velog.io/@lhb7021' target='_blank'>
+          <FormOutlined className="footerIcon" />
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
 
-        <span className="footerName">유지원 |</span>
-        <GithubOutlined className="footerIcon" />
-        <FormOutlined className="footerIcon" />
-        <YoutubeOutlined className="footerIcon" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div className="footerUser3">
+          <span className="footerName">유지원 |</span>
+          <a href='https://github.com/jiweon21' target='_blank'>
+            <GithubOutlined className="footerIcon" />
+          </a>
+          <a href='https://velog.io/@jiwon22' target='_blank'>
+          <FormOutlined className="footerIcon" />
+          </a>
+          <YoutubeOutlined className="footerIcon" />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
 
-        <span className="footerName">임현택 |</span>
-        <a href='https://www.naver.com' target='_blank'>
-          <GithubOutlined className="footerIcon" />
-        </a>
-
-        <FormOutlined className="footerIcon" />
-        <InstagramOutlined className="footerIcon" />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div className="footerUser4">
+          <span className="footerName">임현택 |</span>
+          <a href='https://github.com/Captainjack-kor' target='_blank'>
+            <GithubOutlined className="footerIcon" />
+          </a>
+          <a href='https://velog.io/@iooi75' target='_blank'>
+          <FormOutlined className="footerIcon" />
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
       </div>
     </>
   )
