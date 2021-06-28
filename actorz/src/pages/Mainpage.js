6e2762e157ff20266ddb8cf980cf74540d9fc3a4
Jack Lim <jennystr233@gmail.com>
2021-06-28 14:21:06 +0900

    update mainpage

--- actorz/src/pages/Mainpage.js
+++ actorz/src/pages/Mainpage.js
@@ -10,29 +10,30 @@ const Mainpage = () => {
   return (
     <>
 
-      <div className="blockhere">- </div>
+      <div className="blockhere"> </div>
       <div className="mainPage">
         <Nav />
-        <div className="leftSpace"> </div>
+        <div className="leftSpace"> 
+          <div className="iconList"> 
+          </div>
+        </div>
+
+        <div className="newblockPosition"> </div>
+
         <div className="middleSpace"> 
-
           <div className="midContents">
-            <a href='https://www.naver.com' target='_blank'> 
-              <div className="user"> 
-                송중기
-              </div>
-            </a>
-
-            <button>게시물 작성 </button>
-            <button>마이 페이지 </button>
-
             <img src="https://media.vlpt.us/images/iooi75/post/cb112766-1934-4282-87ed-9c6384afa7e8/image.png" alt=""
                   className="exampleIMG"
             />
-
           </div>
         </div>
-        <div className="rightSpace"> </div>
+        <div className="newblockPosition2"> </div>
+
+        <div className="rightSpace"> 
+          <div className="iconList2">
+          </div>
+        </div>
+        
       </div>
     </>
   );
