7a0622d0a75276593aea59d9c4680466fdd2002b
Jack Lim <jennystr233@gmail.com>
2021-06-28 13:09:06 +0900

    add mainpage

--- actorz/src/pages/Mainpage.js
+++ actorz/src/pages/Mainpage.js
@@ -1,16 +1,36 @@
 import React from "react";
 import Nav from "../components/Nav";
-import "../mainpage.css";
 import Post from "../pages/Post";
 import Mypage from "../pages/Mypage";
-import MypageEdit from "./MypageEdit";
+import '../mainpage.css';
 
 const Mainpage = () => {
   return (
     <>
-      {/* <Nav />
-      <div>메인페이지</div> */}
-      <Mypage />
+      <div className="blockhere">- </div>
+      <div className="mainPage">
+        <Nav />
+        <div className="leftSpace"> </div>
+        <div className="middleSpace"> 
+
+          <div className="midContents">
+            <a href='https://www.naver.com' target='_blank'> 
+              <div className="user"> 
+                송중기
+              </div>
+            </a>
+
+            <button>게시물 작성 </button>
+            <button>마이 페이지 </button>
+
+            <img src="https://media.vlpt.us/images/iooi75/post/cb112766-1934-4282-87ed-9c6384afa7e8/image.png" alt=""
+                  className="exampleIMG"
+            />
+
+          </div>
+        </div>
+        <div className="rightSpace"> </div>
+      </div>
     </>
   );
 };
