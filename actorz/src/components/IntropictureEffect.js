2565c0ed72d37efb0c4cb0df9a28aa2ca5df4e73
jiweon21 <jna215843@gmail.com>
2021-06-30 22:20:06 +0900

    edit:postupload

--- actorz/src/components/IntropictureEffect.js
+++ actorz/src/components/IntropictureEffect.js
@@ -1,172 +1,237 @@
-import React from 'react'
-import { ArrowDownOutlined, UserOutlined, IdcardOutlined, HeartOutlined, FileAddOutlined, HomeOutlined, GithubOutlined, ToolOutlined, InstagramOutlined, FormOutlined, YoutubeOutlined} from '@ant-design/icons';
+import React from "react";
+import {
+  ArrowDownOutlined,
+  UserOutlined,
+  IdcardOutlined,
+  HeartOutlined,
+  FileAddOutlined,
+  HomeOutlined,
+  GithubOutlined,
+  ToolOutlined,
+  InstagramOutlined,
+  FormOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
 
 const IntropictureEffect = () => {
-  return(
+  return (
     <div className="actorCardposition">
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/2b4cc4a3-a7b7-42e6-b1df-516c4cf09264/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/2b4cc4a3-a7b7-42e6-b1df-516c4cf09264/image.png"
+          alt=""
           className="actor1995"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/927f6e62-56ac-4ad8-96c2-98b301819b78/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/927f6e62-56ac-4ad8-96c2-98b301819b78/image.png"
+          alt=""
           className="actor1996"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/5b7ac3a8-eb82-4fc6-9b68-f55f7d98870b/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/5b7ac3a8-eb82-4fc6-9b68-f55f7d98870b/image.png"
+          alt=""
           className="actor1997"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/4ab81f7e-2cc1-4215-bff5-615a926d5fda/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/4ab81f7e-2cc1-4215-bff5-615a926d5fda/image.png"
+          alt=""
           className="actor1998"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/f095e95d-1d92-4b2b-876e-8756123c972c/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/f095e95d-1d92-4b2b-876e-8756123c972c/image.png"
+          alt=""
           className="actor1999"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/cd69845d-17e5-44dd-82f1-b6d3d2305c2a/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/cd69845d-17e5-44dd-82f1-b6d3d2305c2a/image.png"
+          alt=""
           className="actor2000"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/29ea4238-2775-433a-9e03-1d615fe50434/image.png"
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/29ea4238-2775-433a-9e03-1d615fe50434/image.png"
           className="actor2001"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/a72a3f00-cd2d-484c-b80a-3d578d2d1313/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/a72a3f00-cd2d-484c-b80a-3d578d2d1313/image.png"
+          alt=""
           className="actor2002"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/516f6d70-b23e-498c-b9de-1641b4e1090f/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/516f6d70-b23e-498c-b9de-1641b4e1090f/image.png"
+          alt=""
           className="actor2003"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/64c633f5-1b5a-4158-9cd8-cf0528a76503/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/64c633f5-1b5a-4158-9cd8-cf0528a76503/image.png"
+          alt=""
           className="actor2004"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/200568dc-995a-4a16-a74a-be7978496a4f/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/200568dc-995a-4a16-a74a-be7978496a4f/image.png"
+          alt=""
           className="actor2005"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/561c3620-0658-4772-8703-9664a25f50ad/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/561c3620-0658-4772-8703-9664a25f50ad/image.png"
+          alt=""
           className="actor2006"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/201c7920-da83-49c0-89fa-b039b94a6eb2/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/201c7920-da83-49c0-89fa-b039b94a6eb2/image.png"
+          alt=""
           className="actor2007"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/150a3db2-0ebc-4a60-9220-b319300f9a41/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/150a3db2-0ebc-4a60-9220-b319300f9a41/image.png"
+          alt=""
           className="actor2008"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/a4daa1b4-2ee1-461f-8f35-6ed1b73bf7bd/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/a4daa1b4-2ee1-461f-8f35-6ed1b73bf7bd/image.png"
+          alt=""
           className="actor2009"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/421e4542-680c-4f90-aa33-9a916125899e/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/421e4542-680c-4f90-aa33-9a916125899e/image.png"
+          alt=""
           className="actor2010"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/5e398436-b950-472f-aed6-2fc52939df11/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/5e398436-b950-472f-aed6-2fc52939df11/image.png"
+          alt=""
           className="actor2011"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/d4f5160b-e244-4897-a8fd-71f65ade6e32/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/d4f5160b-e244-4897-a8fd-71f65ade6e32/image.png"
+          alt=""
           className="actor2012"
         />
       </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/0909c380-910d-478b-83e4-a0af471feff4/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/0909c380-910d-478b-83e4-a0af471feff4/image.png"
+          alt=""
           className="actor2013"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/2d18320d-c478-4dc4-8d13-9c76499f1a43/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/2d18320d-c478-4dc4-8d13-9c76499f1a43/image.png"
+          alt=""
           className="actor2014"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/cb112766-1934-4282-87ed-9c6384afa7e8/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/cb112766-1934-4282-87ed-9c6384afa7e8/image.png"
+          alt=""
           className="actor2015"
         />
-      </div> 
-      
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/14bfeab1-d5c0-47ba-a07e-3df549855794/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/14bfeab1-d5c0-47ba-a07e-3df549855794/image.png"
+          alt=""
           className="actor2016"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/4e2a145e-44a7-4fa3-b148-37c24cb524c8/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/4e2a145e-44a7-4fa3-b148-37c24cb524c8/image.png"
+          alt=""
           className="actor2017"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/fdd1ff15-cecb-48ba-b3cc-d204672ae028/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/fdd1ff15-cecb-48ba-b3cc-d204672ae028/image.png"
+          alt=""
           className="actor2018"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/d046275e-8720-4544-9116-0fc9d6b69395/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/d046275e-8720-4544-9116-0fc9d6b69395/image.png"
+          alt=""
           className="actor2019"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/0125cdd7-a68b-45b6-a55a-1ac4d28ef975/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/0125cdd7-a68b-45b6-a55a-1ac4d28ef975/image.png"
+          alt=""
           className="actor2020"
         />
-      </div> 
+      </div>
 
       <div data-trigger>
-        <img src="https://media.vlpt.us/images/iooi75/post/2690032c-17d3-44bb-bcc0-ba32b00babf4/image.png" alt=""
+        <img
+          src="https://media.vlpt.us/images/iooi75/post/2690032c-17d3-44bb-bcc0-ba32b00babf4/image.png"
+          alt=""
           className="actorLastimage"
         />
       </div>
+    </div>
   );
-}
+};
 
 export default IntropictureEffect;
