88dbdf339ad50068a4b3ac147f3ce6bd90903cf3
Jack Lim <jennystr233@gmail.com>
2021-07-01 02:44:12 +0900

    update mypageEdit

--- actorz/src/pages/Posts.js
+++ actorz/src/pages/Posts.js
@@ -112,64 +112,7 @@ const Posts = () => {
             </div>
           </div>
         <Footer />
-      
-      {/* <Nav />
-      <div id="post-container1"></div>
-      <div id="post-title">Actor</div>
-      <div id="container">
-        <img src={img} className="img" alt="프로필"></img>
-        <span id="post-info">
-          <p className="name">{user.data.userInfo.name}</p>
-          <ul>
-            <strong>생년월일</strong>
-            <li className="dob">{user.data.userInfo.dob}</li>
-            <strong>이메일</strong>
-            <li className="email">{user.data.userInfo.email}</li>
-            <strong>소속사</strong>
-            <li className="company">{user.data.userInfo.company}</li>
-          </ul>
-        </span>
-      </div>
-      <span className="post-career">
-        {user.data.userInfo.careers.map((career) => {
-          return (
-            <li>
-              {`${career.year}` +
-                ` ${career.title}` +
-                ` / ` +
-                `${career.type.map((type) => {
-                  return type;
-                })}`}
-            </li>
-          );
-        })}
-      </span>
-      {
-        <Slider {...settings} className="slider">
-          {post.data.posts.map((post) => {
-            return (
-              <img
-                src={post.path}
-                onClick={() => handleClickPost(true, post.id)}
-              ></img>
-            );
-          })}
-        </Slider>
-      }
-      {clickModal ? <Post handleClickPost={handleClickPost} /> : null} */}
     </>
   );
 };
 export default Posts;
-
-/* 
-
- <Link
-                to={{
-                  pathname: `/post/${post.id}`,
-                  state: { id: post.id },
-                }}
-              >
-                <img src={post.path} />
-              </Link>
-*/
