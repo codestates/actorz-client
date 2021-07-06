19fb084cdb9361ee666ffb28ea7c9c3ed0b57a57
Jack Lim <jennystr233@gmail.com>
2021-07-06 10:47:59 +0900

    fixed git

--- actorz/src/apis/server.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const baseURL =
-  process.env.NODE_ENV === "production" ? "/api" : "https://localhost:3001/api";
-
-export default axios.create({
-  baseURL,
-  withCredentials:true
-});
