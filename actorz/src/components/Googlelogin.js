e273760aa9137b2350237d5340a705a893b0ce38
SundeulDonaKim <kphddn@gmail.com>
2021-07-07 10:49:27 +0900

    #22:edit:oauth

--- actorz/src/components/Googlelogin.js
+++ actorz/src/components/Googlelogin.js
@@ -2,23 +2,36 @@ import React from 'react';
 import Googlelogin from 'react-google-login';
 import server from '../apis/server';
 import google from "../images/google.png";
+import { getUserInfo } from "../actions/userAction";
+import { useDispatch } from "react-redux";
 
 const Google = ({ handleClickClose }) => {
-
+  const dispatch = useDispatch();
+  
   const responseGoogle = async (googleData) => {
     await server.post("/login/google", {
       token: googleData.tokenId
     }).then(res => {
-      if (res.status === 200) {
+      if (res.status === 200 || res.status === 201) {
         localStorage.setItem("accessToken", res.data.data.accessToken);
         handleClickClose();
-      } else if (res.status === 201) {
-        localStorage.setItem("accessToken", res.data.data.accessToken);
-        handleClickClose();
-        // edit needed
       } else {
         alert("구글 로그인 중 오류가 발생했습니다.");
       }
+    });
+    await server //로그인한 유저의 정보를 state에 저장
+    .get(`/user/:user_id`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        dispatch(getUserInfo(res.data.data.userInfo));
+      }
+    })
+    .catch((err) => {
+      throw err;
     });
     // console.log(res);
     console.log('---------------------')
--- actorz/src/pages/LoginCallback.js
+++ actorz/src/pages/LoginCallback.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { getUserInfo } from "../actions/userAction";
+import { useDispatch } from "react-redux";
 import '../App.css'
 import 'antd/dist/antd.css';
 
@@ -6,24 +8,45 @@ import 'antd/dist/antd.css';
 import server from "../apis/server";
 
 const LoginCallback = () => {
+  const dispatch = useDispatch();
+
   const login = async () => {
     try{
       const location = window.location.href.split("=")[1];
       const code = location.split("&")[0];
-      const statusCode = await server.post("/login/naver", { code }).then(res => res.status);
-      console.log(statusCode)
-      if(statusCode === 201){
-        
-      }
+      await server.post("/login/naver", { code }).then(res => {
+        if (res.status === 200 || res.status === 201) {
+          localStorage.setItem("accessToken", res.data.data.accessToken);
+        } else {
+          alert("구글 로그인 중 오류가 발생했습니다.");
+        }
+      });
+
+      await server //로그인한 유저의 정보를 state에 저장
+          .get(`/user/:user_id`, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              dispatch(getUserInfo(res.data.data.userInfo));
+            }
+          })
+          .catch((err) => {
+            throw err;
+          });
+
+      window.location.href = "https://localhost:3000/mainpage"
     }catch(err){
       console.log(err);
     }
   }
 
-  useEffect(() => {login()}, [])
+  useEffect(() => {login()})
   
   return (
-    <div>helloWorld</div>
+    <div></div>
   )
 }
 export default LoginCallback;
