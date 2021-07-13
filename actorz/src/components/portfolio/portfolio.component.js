e63f6d63a942d25880df47262cf569045febf5c6
lhb7021 <manevil6@gmail.com>
2021-07-13 17:07:10 +0900

    edit:pfCSS

--- actorz/src/components/portfolio/portfolio.component.js
+++ actorz/src/components/portfolio/portfolio.component.js
@@ -1,31 +1,30 @@
 import React, { useRef, useState, useEffect } from "react";
 import {
-  FileUploadContainer,
+  PfUploadContainer,
   FormField,
   DragDropText,
-  FilePreviewContainer,
+  PostPreviewContainer,
   ImagePreview,
   VideoPreview,
+  PostMetaData,
   PreviewContainer,
   PreviewList,
-  FileMetaData,
-  RemoveFileIcon,
+  RemovePostIcon,
   InputLabel,
 } from "./portfolio.styles";
 import "../../styles/Postupload.css";
 import Loading from "../loading";
 
-
-
 const PortfolioEdit = ({
   handleClickPfEdit,
+  myPostsData,
   isLoading
 }) => {
   const [postsData, setpostsData] = useState([]);
 
   useEffect(() => {
-
-  }, [])
+    console.log(myPostsData);
+  }, [myPostsData])
 
   return (
     <>
@@ -33,11 +32,39 @@ const PortfolioEdit = ({
         onClick={() => handleClickPfEdit(false)} >
         <div id="upload-modal-container"
         onClick={(event) => event.stopPropagation()}>
-          <FileUploadContainer>
+          <PfUploadContainer>
             <InputLabel>
-              포트폴리오에 올릴 사진 또는 동영상을 선택해주세요
+              포트폴리오에 올릴 게시물을 선택해주세요
             </InputLabel>
-          </FileUploadContainer>
+
+            <PostPreviewContainer>
+              <PreviewList>
+                {myPostsData.map((post, index) => {
+                  return (
+                    <PreviewContainer key={index}>
+                      <div>
+                        {post.media[0].type === "img"
+                        ? <ImagePreview src={post.media[0].path}/> 
+                        : <VideoPreview src={post.media[0].path}/>}
+                        {/* <PostMetaData isMediaFile={true}>
+                          <aside>
+                            <RemovePostIcon
+                              className="fas fa-trash-alt"
+                              onClick={() => console.log("remove")}
+                            />
+                          </aside>
+                        </PostMetaData> */}
+                      </div>
+                    </PreviewContainer>
+                  )
+                })}
+
+
+              </PreviewList>
+            </PostPreviewContainer>
+
+
+          </PfUploadContainer>
           <div className="desc-box">
           </div>
 
