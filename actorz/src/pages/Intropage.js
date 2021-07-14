cdc336f4c92710a4116d31371cad1cc2ded9900b
Jack Lim <jennystr233@gmail.com>
2021-07-14 11:48:18 +0900

    add intropageResponsive

--- actorz/src/pages/Intropage.js
+++ actorz/src/pages/Intropage.js
@@ -10,8 +10,10 @@ import IntroNav from "../components/IntroNav";
 import ScrollTrigger from '@terwanerik/scrolltrigger';
 import ResponsiveApp from "../components/responsiveApp/ResponsiveNav"
 import Footer from "../components/Footer"
+import ResponsiveFooter from "../components/responsiveApp/ResponsiveFooter";
 const trigger = new ScrollTrigger();
 trigger.add('[data-trigger]');
+
 
 
 const Intropage = () => {
@@ -20,13 +22,79 @@ const Intropage = () => {
     query : "(min-width:1024px)"
   });
 
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
+
   const isMobile = useMediaQuery({
     query : "(max-width:767px)"
   });
 
   return (
     <>
-      {isPc && <div>PC반응형
+      {isPc && <div>
+        <IntroNav />
+        <div className="backgroundImage">
+          {/* <IntroNav /> */}
+          <div className="blockPosition"></div>
+          <div className="introContainer">
+            <div id="logoPosition">
+              <img src="https://media.vlpt.us/images/iooi75/post/08503e87-6d66-4e92-948b-58988f10b27a/Screen%20Shot%202021-06-22%20at%209.24.46%20PM.png" alt=""
+                className="projectLogo"
+              />
+            </div>
+            <div className="mainIntro1">매번 똑같은 포트폴리오 양식</div>
+              <br></br>
+            <div className="mainIntro2">나만의 특별한 포트폴리오에 사진과 영상을 담아 남들과 다른 포트폴리오를 만들어 드리겠습니다.</div>
+              <br></br><br></br><br></br>
+            <div className="button1Position">
+              <Link to="/mainpage">
+                <Button type="primary" className="startButton1" danger>시작하기</Button>
+              </Link>
+            </div>
+            <div className="scrollEffect">
+              스크롤을 내려주세요!
+            </div>
+            <ArrowDownOutlined className="scrollIcon"/>
+          </div>
+        </div>
+        <div className="blockPosition2">
+          <div className="blockPosition3"> </div>
+          <div className="middleContents"> </div>
+          <div className="blockPosition4"> </div>
+          <div className="middleContents2"> 
+              <div className="blackSpace01"> </div> 
+
+              <div className="actorCardposition">
+              
+              </div>
+              <div className="blackSpace02">  </div>
+          </div>
+        </div>
+
+        <div className="lastContents"> 
+          <div className="lastContext">
+            다음 2022년 시상식의 주인공은 바로 여러분입니다!
+          </div>
+          <div className="lastContext">
+            Acotrz는 여러분의 꿈을 도와 드리겠습니다.
+          </div>
+          </div>
+
+        <div className="button2Position">
+          <Link to="/mainpage">
+            <Button type="primary" className="startButton2" danger>시작하기</Button>
+          </Link>
+        </div>
+        <br></br><br></br><br></br><br></br>
+
+        
+        <Footer />
+
+      </div>}
+      
+      {isTablet && 
+        <>
         <IntroNav />
         <div className="backgroundImage">
           {/* <IntroNav /> */}
@@ -84,9 +152,8 @@ const Intropage = () => {
 
         <Footer />
 
-
-      </div>}
-      
+        </>
+      }
 
       {isMobile && <>
         <ResponsiveApp />
